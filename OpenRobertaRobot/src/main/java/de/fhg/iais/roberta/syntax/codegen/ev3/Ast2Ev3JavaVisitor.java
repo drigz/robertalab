56815c57de57a26159adb4c66647f58c5975296a
Nehru <bnehru@iais.fraunhofer.de>
2016-06-13 13:11:45 +0200

    #128 some changed files

--- OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/syntax/codegen/ev3/Ast2Ev3JavaVisitor.java
+++ OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/syntax/codegen/ev3/Ast2Ev3JavaVisitor.java
@@ -2211,10 +2211,6 @@ public class Ast2Ev3JavaVisitor implements AstVisitor<Void> {
                     this.sb.append("} \n");
                     this.incrIndentation();
                     break;
-                case TURN_RIGHT:
-                    this.sb.append("#define turn_right(s, t)");
-
-                    break;
 
                 /*
                 case JTEXT:
@@ -2247,7 +2243,10 @@ public class Ast2Ev3JavaVisitor implements AstVisitor<Void> {
             switch ( customFunction ) {
                 case EXP:
                     this.sb.append("#define E 2.71828 \n");
-
+                case TURN_RIGHT:
+                    this.sb.append("#define turn_right(s,t) \n");
+                case TURN_LEFT:
+                    this.sb.append("#define turn_left(s,t) \n");
             }
 
         }
@@ -2304,15 +2303,15 @@ public class Ast2Ev3JavaVisitor implements AstVisitor<Void> {
         sb.append(INDENT).append(INDENT).append(INDENT).append("    .build();");
         return sb.toString();
     }
-
-
+    
+    
     private void appendSensors(StringBuilder sb) {
         for ( Map.Entry<SensorPort, EV3Sensor> entry : this.brickConfiguration.getSensors().entrySet() ) {
             sb.append(INDENT).append(INDENT).append(INDENT);
             appendOptional(sb, "    .addSensor(", entry.getKey(), entry.getValue());
         }
     }
-
+    
     private void appendActors(StringBuilder sb) {
         for ( Map.Entry<ActorPort, EV3Actor> entry : this.brickConfiguration.getActors().entrySet() ) {
             sb.append(INDENT).append(INDENT).append(INDENT);
@@ -2333,8 +2332,8 @@ public class Ast2Ev3JavaVisitor implements AstVisitor<Void> {
             sb.append(")\n");
         }
     }
-
-
+    
+    
     private String generateRegenerateUsedSensors() {
         StringBuilder sb = new StringBuilder();
         String arrayOfSensors = "";
@@ -2342,7 +2341,7 @@ public class Ast2Ev3JavaVisitor implements AstVisitor<Void> {
             arrayOfSensors += usedSensor.generateRegenerate();
             arrayOfSensors += ", ";
         }
-
+    
         sb.append("private Set<UsedSensor> usedSensors = " + "new LinkedHashSet<UsedSensor>(");
         if ( this.usedSensors.size() > 0 ) {
             sb.append("Arrays.asList(" + arrayOfSensors.substring(0, arrayOfSensors.length() - 2) + ")");
@@ -2362,14 +2361,14 @@ public class Ast2Ev3JavaVisitor implements AstVisitor<Void> {
         sb.append(", ").append(getEnumCode(ev3Actor.getRotationDirection())).append(", ").append(getEnumCode(ev3Actor.getMotorSide())).append(")");
         return sb.toString();
     }
-
+    
     private static String generateRegenerateEV3Sensor(HardwareComponent sensor) {
         StringBuilder sb = new StringBuilder();
         sb.append("new EV3Sensor(").append(getHardwareComponentTypeCode(sensor.getComponentType()));
         sb.append(")");
         return sb.toString();
     }
-
+    
     private static String getHardwareComponentTypeCode(HardwareComponentType type) {
         return type.getClass().getSimpleName() + "." + type.getTypeName();
     }
