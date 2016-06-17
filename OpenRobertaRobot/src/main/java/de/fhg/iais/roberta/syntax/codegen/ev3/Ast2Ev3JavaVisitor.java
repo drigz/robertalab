e423a92c672f5ea1cccd54a8681cf3562984a1a2
brindha <brindha.lara25@gmail.com>
2016-06-17 12:38:23 +0200

    some tested changes

--- OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/syntax/codegen/ev3/Ast2Ev3JavaVisitor.java
+++ OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/syntax/codegen/ev3/Ast2Ev3JavaVisitor.java
@@ -2264,7 +2264,7 @@ public class Ast2Ev3JavaVisitor implements AstVisitor<Void> {
         this.sb.append("#include \"hal.h\" \n");
         this.sb.append("#include \"NXCDefs.h\" \n");
         //TODO: change it to remove custom function visitor
-
+        //  added constants for turnaction
         for ( final FunctionNames customFunction : this.usedFunctions ) {
 
             switch ( customFunction ) {
